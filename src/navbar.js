d8ef12e8236502a9fe8991f987cadcfae459f81a
Vicky Chen <54949161+vickychenche@users.noreply.github.com>
2022-10-01 17:29:32 -0400

    Add files via upload

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+export default function navbar(){
+    return <nav className="nav">
+        <a href="/" class="site-title"> Vicky Chen</a>
+        <ul>
+            <li>
+                <a href="/about"> about me</a>
+            </li>
+            <li>
+                <a href="/work-experience"> Work Experience</a>
+            </li>
+        </ul>
+    </nav>
+}
