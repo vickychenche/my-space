b191a687f0e93c989390ba4624a7053ca26c5e82
Vicky Chen <vicky20020103@gmail.com>
2022-10-01 20:28:09 -0400

    here

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+import pfp from "../images/Vicky.png";
+export default function Home(){
+
+    return( 
+        <>
+        <img src={pfp} />
+        <body> 
+            Hello, I am Vicky! <br></br>
+            Computer Science Specialist + Biology Minor
+        </body>
+        </>
+        
+    );
+}
