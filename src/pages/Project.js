f591cfedc37d79cc1d2e392a6fc01484c86e8a7e
Vicky Chen <54949161+vickychenche@users.noreply.github.com>
2022-10-03 19:59:10 -0400

    Add files via upload

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,3 +1,18 @@
 export default function Project(){
-    return<h1>Project </h1>
+    return(
+        <>
+        <h1>Project </h1>
+        <h2>The following are some of the projects I have worked on throughout the years!</h2>
+        <h3> Three Musketeer (Fall 2021)</h3>
+        <body>
+        This was a project that I made with 3 other classmates when I took CSC207 (software design) class. <br></br>
+        We created a chest like game that allowed 2 types of playstyle, user-user, user-computer. <br></br>
+        The project was done in java with agile cycle and used git heavly. <br></br>
+        There were a minimun of 6 differnet OOD design ex. MVC, Strategy, Builder, Iterator, composite, singleton, that was used in this project<br></br>
+        </body>
+        
+        
+        </>
+    
+    )
 }
